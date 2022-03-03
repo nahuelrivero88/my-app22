699b69dd9a3e6b0c38c503dd98e4ab9f95b917a0
Nahuel Rivero <nahuelriveroch@gmail.com>
2022-03-02 22:20:13 -0300

    Desafio 2 - Menú e-commerce

---
+++ src/Components/NavBar.js
@@ -0,0 +1,17 @@
+export const NavBar = () => {
+
+    
+    return (
+    
+    <header className="header">
+        <h1 className="titulo">Rivero Store</h1>
+        <nav>
+            <ul className = "nav">
+                <li>Inicio</li>
+                <li>Productos</li>
+                <li>Contacto</li>
+            </ul>
+        </nav>
+    </header>
+    )
+}
